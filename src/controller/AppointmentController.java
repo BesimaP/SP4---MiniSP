8bd85777fc8f40fd0350954d9198686b5ec1824c
BesimaP <bepl1000@stud.ek.dk>
2026-05-11 11:04:30 +0200

    AppointmentController & NewRoundController er oprettet, klassediagram opdateret

---
+++ src/controller/AppointmentController.java
@@ -0,0 +1,18 @@
+package controller;
+
+    public class AppointmentController {
+        // Køres når brugeren klikker Tilføj Aftale
+        public void handleAddAppointment() {
+
+        }
+
+        // Køres når brugeren markerer en aftale som gennemført
+        public void handleMarkCompleted() {
+
+        }
+
+        // Køres når brugeren klikker Gem
+        public void handleSave() {
+
+        }
+    }
