7ad96dc4897cca63c04d30d5a1795e8ca3e03053
BesimaP <bepl1000@stud.ek.dk>
2026-05-11 17:21:20 +0200

    HormoneLog.java, HormoneLogController, MedicationLog og MedicationLogController indsat

--- src/model/PsychiatryJourney.java
+++ src/model/PsychiatryJourney.java
@@ -4,20 +4,15 @@ import enums.Status;
 import java.time.LocalDate;
 
 public class PsychiatryJourney extends Journey{
-    private String injuryType;
-    private String goal;
+    private String condition;
 
-    public PsychiatryJourney(LocalDate StartDate, Status status, String injuryType, String goal){
-        super(StartDate, status);
-        this.injuryType = injuryType;
-        this.goal = goal;
+    public PsychiatryJourney(LocalDate startDate, Status status, String condition){
+        super(startDate, status);
+        this.condition = condition;
     }
 
     //Getters
-    public String getInjuryType(){
-        return injuryType;
-    }
-    public String getGoal(){
-        return goal;
+    public String getCondition(){
+        return condition;
     }
 }
