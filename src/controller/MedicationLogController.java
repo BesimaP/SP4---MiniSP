fc89d0fa86dff89d1c2b959f7d366f17c96b9677
BesimaP <bepl1000@stud.ek.dk>
2026-05-11 16:25:22 +0200

    HormoneLog.java, HormoneLogController, MedicationLog og MedicationLogController indsat

---
+++ src/controller/MedicationLogController.java
@@ -0,0 +1,15 @@
+package controller;
+
+// Håndterer tilføjelse og gemning af medicinindtastninger
+public class MedicationLogController {
+
+    // Køres når brugeren klikker Tilføj Medicin
+    public void handleAddMedication() {
+
+    }
+
+    // Køres når brugeren klikker Gem
+    public void handleSave() {
+
+    }
+}
