1386f8e735e39b85d4d94546102300337eb607b6
louiseasverdrup <losv1000@stud.ek.dk>
2026-05-13 14:44:30 +0200

    medication view

--- src/view/EndRoundView.java
+++ src/view/EndRoundView.java
@@ -1,4 +1,20 @@
 package view;
 
+import controller.EndRoundController;
+import javafx.stage.Stage;
+
 public class EndRoundView {
+    private EndRoundController controller = new EndRoundController();
+
+    public void show(Stage stage){
+
+
+
+
+
+    }
+
+
+
+
 }
