71f69096ba4367f94ccc477be70966b5f0757a6d
louiseasverdrup <losv1000@stud.ek.dk>
2026-05-13 09:57:29 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import model.DatabaseConnection;
 import model.DatabaseInitializer;
+import model.Patient;
+import view.ProfileView;
 import view.StartSystemView;
 
 public class Main extends Application {
